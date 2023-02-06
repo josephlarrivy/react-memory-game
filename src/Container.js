d31eafc61e60f3721d03ce99eb3f8c225f4d74fe
josephlarrivy <joseph.larrivy@gmail.com>
2023-02-06 10:22:53 -0600

    getting things set up

---
+++ src/Container.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Square from "./Square";
+
+const Container = () => {
+
+
+    return (
+        <div>
+            <Square/>
+        </div>
+    )
+}
+
+export default Container;
---
+++ src/Square.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Square = () => {
+
+
+    return (
+        <div>
+            <p>square</p>
+        </div>
+    )
+}
+
+export default Square;
